ca70c481fcf6a59030032df96c7e93ebc8ab3197
Pato-PC <sergiopatohumanes@gmail.com>
2018-04-18 21:49:39 +0200

    Login +- y Global fin
    He añadido un login auxiliar, aun falta que coja los datos del formulario, pero se puede trastear ya con el cambiando los parametros en la funcion check_login();

--- practica02/javascript.js
+++ practica02/javascript.js
@@ -217,7 +217,139 @@ function detalle_receta(url){
 				});
 			})
 }
+//FUNCIONES NUEVA RECETA
+function red_nuevareceta(){
+	if(!logueado()){
+		location.href="index.html";
+	}
+}
+//FIN FUNCIONES NUEVA RECETA
 
+//FUNCIONES DEL MENU
+
+function dibujar_menu(){
+	var element;
+	element = document.getElementById("menu-principal");
+	
+	if(element){
+		if(logueado()){
+			element.innerHTML = 
+			'<a href="index.html">Inicio <i class="fas fa-home"></i></a> ' +
+			'<a href="buscar.html">Buscar <i class="fas fa-search"></i></a> ' +
+			'<a href="nueva-receta.html" id="n_rec">Nueva receta <i class="fas fa-plus-circle"></i></a> ' +
+			'<a href="index.html" id="logout" onclick="desconectar();">Logout <i class="fas fa-sign-out-alt"></i></a> ';
+		}
+		else{
+			element.innerHTML = 
+			'<a href="index.html">Inicio <i class="fas fa-home"></i></a> ' +
+			'<a href="buscar.html">Buscar <i class="fas fa-search"></i></a> ' +
+			'<a href="registro.html" id="registro">Registro <i class="fas fa-user-plus"></i></a> ' +
+			'<a href="login.html" id="login" >Login <i class="fas fa-sign-in-alt"></i></a> ' ;
+		}
+	}
+
+}
+
+//FIN FUNCIONES DEL MENU
+//FUNCIONES DE LOGIN
+
+function check_login(){
+	let url = 'http://localhost/PCW/practica02/rest/login/',
+	fd = new FormData(),
+	init = {method:'post', 'body':fd};
+	
+	/*
+	let usuario = document.getElementById("usuario").value;
+	let contrasenya = document.getElementById("pass").value;
+	
+	console.log(usuario);
+	console.log(contrasenya);
+	*/
+	fd.append('login', "usuario2");
+	fd.append('pwd', "usuario2");
+
+
+	fetch(url,init).then(function(response){
+		if(!response.ok){
+			console.log('Usuario o contrasenya incorrecto');
+			location.href ="login.html?inco=ok";
+			return;
+		}
+		response.json().then(function(data){
+			console.log("todo oki");
+			if(window.sessionStorage){
+				console.log(data);
+				sessionStorage.setItem('usuario', JSON.stringify(data));
+			}
+			
+			location.href="index.html";
+		});
+	})/*.cath(function(err){
+	console.log('Fetch Error: ', err);
+	})*/;
+}
+
+function logueado(){
+	if(window.sessionStorage){
+		if(sessionStorage.getItem('usuario')){
+			let dato =  sessionStorage.getItem('usuario') ;
+			let data = JSON.parse(dato);
+			console.log(data.login + " logueado");
+			console.log(data);
+			return true;
+		}
+		else{
+			console.log("nadie logueado");
+			return false;
+		}
+	}
+}
+
+function mensaje_incorrecto(){
+	var parametros = getParameterByName('inco');
+	if(getParameterByName('inco')){
+		var element;
+		element = document.getElementById("login-errorjs");
+		if (element) {
+			element.innerHTML = 
+			'<div id="login-error">' +
+			'<p>Usuario o contraseña incorrecto</p>' +
+			'<p><button onclick="cerrar_aviso();"><i class="far fa-times-circle"></i></button></p>'+
+			'</div>';
+		}
+	}
+	
+}
+function cerrar_aviso(){
+	var element;
+		element = document.getElementById("login-errorjs");
+		if (element) {
+			element.innerHTML = '';
+		}
+}
+
+function red_login_registro(){
+	if(logueado()){
+		location.href="index.html";
+	}
+}
+
+//FIN DE FUNCIONES DE LOGIN
+
+//LOGOUT
+
+function desconectar(){
+	if(window.sessionStorage){
+		sessionStorage.clear();
+	}
+}
+
+
+//FUNCIONES DE REGISTRO
+
+//FIN FUNCIONES REGISTRO
+	
+//FUNCIONES EXTERNAS 
 
 
 function getParameterByName(name){
@@ -227,50 +359,11 @@ function getParameterByName(name){
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-
-function comprobar(){
-	if(window.localStorage){
-		var form=document.querySelectorAll("form")[0];
-
-		if(form.check.checked){
-			localStorage.setItem("login",form.login.value);
-
-		}
-	}
-}
-
-function rellenar(){
-	if(window.localStorage){
-		var form=document.querySelectorAll("form")[0];
-
-		if(localStorage.getItem("login")){
-			form.login.value=localStorage.getItem("login");
-			
-		}
-	}
-}
-
-function cabecera(){
-
-	if(window.localStorage){
-		if(!localStorage.getItem("login")){
-			document.getElementById("n_rec").style.display='none';
-			document.getElementById("logout").style.display='none';
-			document.getElementById("login").style.display='relative';
-			document.getElementById("registro").style.display='relative';
-		}
-		else{
-			document.getElementById("n_rec").style.display='relative';
-			document.getElementById("logout").style.display='relative';
-			document.getElementById("login").style.display='none';
-			document.getElementById("registro").style.display='none';
-		}
-
-	}
-
-}
-
-function comprobar_loguin(){
-
-
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 }
