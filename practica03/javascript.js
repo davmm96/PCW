b6bd63f8739cb143c2d5e6684b31f127fc9401df
Pato-PC <sergiopatohumanes@gmail.com>
2018-05-22 16:08:21 +0200

    Subir la foto de las 2 formas

--- practica03/javascript.js
+++ practica03/javascript.js
@@ -12,11 +12,17 @@ function load_canvas(){
 	//derrapa&dropea
 	let cv01 = document.querySelector('#cv_img');
 
+	ctx = cv01.getContext('2d');
+	ctx.font = 'bold 18px sans-serif';
+	ctx.textAlign = 'center';
+	ctx.fillText('Haz click o arrastra una imagen aquí',180,100);
+
 	cv01.ondragover = function(e){
 		e.stopPropagation();
 		e.preventDefault(); //return false;
 		console.log("Drageando");
 	};
+
 
 	cv01.ondrop = function(e){
 		e.stopPropagation();
@@ -28,11 +34,41 @@ function load_canvas(){
 		fr.onload = function(){
 			let img = new Image();
 			img.onload = function(){
-				let ctx = cv01.getContext('2d');
-				ctx.drawImage(img,0,0,cv01.width,cv01.height);
+				console.log(img);
+				insertar_foto_canvas(img);
 			};
 			img.src = fr.result;
 		};
 		fr.readAsDataURL(fichero);
 	};
+}
+
+function anyadir_foto(input){
+	if(input.files[0]){
+
+		let fichero = input.files[0];
+		let fr = new FileReader();
+
+		fr.onload = function(){
+			let img = new Image();
+			img.onload = function(){
+				console.log(img);
+				insertar_foto_canvas(img);
+			};
+			img.src = fr.result;
+		};
+		fr.readAsDataURL(fichero);
+	}
+}
+
+function insertar_foto_canvas(img){
+
+	let cv01 = document.querySelector('#cv_img');
+	let ctx = cv01.getContext('2d');
+	ctx.drawImage(img,0,0,cv01.width,cv01.height);
+
+	let cv02 = document.querySelector('#cv_sel');
+	let ctx2 = cv02.getContext('2d');
+	ctx2.drawImage(img,0,0,cv02.width,cv02.height);
+
 }
