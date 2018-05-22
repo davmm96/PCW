e4ef8bfe995c7ec0aef0c9233a933c74b76f8397
David Melero <dmelero96@gmail.com>
2018-05-22 19:48:28 +0200

    Cambiar color y dificultad

--- practica03/javascript.js
+++ practica03/javascript.js
@@ -1,11 +1,15 @@
 const _ANCHO = 360;
 const _ALTO = 240;
-const img=false;
+var imag=false;
+var dificultad=1;
+var color="#f00";
+var ctx2;
+var imagen;
 
 function load_canvas(){
 
 
-if(!img){
+if(!imag){
 	document.getElementById("start").disabled = true;
 	document.getElementById("finish").disabled = true;
 	document.getElementById("help").disabled = true;
@@ -22,7 +26,7 @@ if(!img){
 	let cv01 = document.querySelector('#cv_img');
 	let cv02 = document.querySelector('#cv_sel');
 
-	ctx = cv01.getContext('2d');
+	let ctx = cv01.getContext('2d');
 	ctx.font = 'bold 18px sans-serif';
 	ctx.textAlign = 'center';
 	ctx.fillText('Haz click o arrastra una imagen aquí',180,100);
@@ -45,13 +49,13 @@ if(!img){
 			let img = new Image();
 			img.onload = function(){
 
-				console.log(img);
 				insertar_foto_canvas(img);
 
 			};
 			img.src = fr.result;
 		};
 		fr.readAsDataURL(fichero);
+		imag=true;
 		document.getElementById("start").disabled = false;
 	};
 }
@@ -65,12 +69,14 @@ function anyadir_foto(input){
 		fr.onload = function(){
 			let img = new Image();
 			img.onload = function(){
-				console.log(img);
+
 				insertar_foto_canvas(img);
 			};
 			img.src = fr.result;
 		};
 		fr.readAsDataURL(fichero);
+		imag=true;
+		document.getElementById("start").disabled = false;
 	}
 }
 
@@ -81,7 +87,88 @@ function insertar_foto_canvas(img){
 	ctx.drawImage(img,0,0,cv01.width,cv01.height);
 
 	let cv02 = document.querySelector('#cv_sel');
-	let ctx2 = cv02.getContext('2d');
+	ctx2 = cv02.getContext('2d');
 	ctx2.drawImage(img,0,0,cv02.width,cv02.height);
 
+	imagen=img;
+
+	ctx2=dibujar_rejilla(ctx2);
+
+	ctx2.strokeStyle=color;
+	ctx2.stroke();
+
+
+}
+
+function cambio_dificultad(dif){
+
+	if(dificultad!=dif.value){
+		dificultad=dif.value;
+
+		let cv02 = document.querySelector('#cv_sel');
+		cv02.width=cv02.width;
+
+		ctx2.drawImage(imagen,0,0,cv02.width,cv02.height);
+		ctx2=dibujar_rejilla(ctx2);
+
+		ctx2.strokeStyle=color;
+		ctx2.stroke();
+	}
+}
+
+
+
+function cambio_color(col){
+	if(color!=col.value){
+		color=col.value;
+
+		let cv02 = document.querySelector('#cv_sel');
+		cv02.width=cv02.width;
+
+		ctx2.drawImage(imagen,0,0,cv02.width,cv02.height);
+
+
+		ctx2=dibujar_rejilla(ctx2);
+
+		ctx2.strokeStyle=color;
+		ctx2.stroke();
+
+	}
+}
+
+function dibujar_rejilla(ctx2){
+	if(dificultad==1){
+			for(var x=0; x<=360; x=x+60){
+				ctx2.moveTo(x,0);
+				ctx2.lineTo(x,240);
+			}
+
+			for(var y=0; y<=240; y=y+60){
+				ctx2.moveTo(0,y);
+				ctx2.lineTo(360,y);
+			}
+	}
+	else if(dificultad==2){
+			for(var x=0; x<=360; x=x+40){
+				ctx2.moveTo(x,0);
+				ctx2.lineTo(x,240);
+			}
+
+			for(var y=0; y<=240; y=y+40){
+				ctx2.moveTo(0,y);
+				ctx2.lineTo(360,y);
+			}
+	}
+	else if(dificultad==3){
+			for(var x=0; x<=360; x=x+30){
+				ctx2.moveTo(x,0);
+				ctx2.lineTo(x,240);
+			}
+
+			for(var y=0; y<=240; y=y+30){
+				ctx2.moveTo(0,y);
+				ctx2.lineTo(360,y);
+			}
+		}
+		return ctx2;
 }
