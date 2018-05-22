d99d1fc584c849c80a2bf663e8febfce305337e5
lfevers <sergiopatohumanes@gmail.com>
2018-05-22 11:52:56 +0200

    Drag and Drop hecho

--- practica03/javascript.js
+++ practica03/javascript.js
@@ -9,5 +9,30 @@ function load_canvas(){
 		e.height 	= _ALTO;
 	});
 
-	
+	//derrapa&dropea
+	let cv01 = document.querySelector('#cv_img');
+
+	cv01.ondragover = function(e){
+		e.stopPropagation();
+		e.preventDefault(); //return false;
+		console.log("Drageando");
+	};
+
+	cv01.ondrop = function(e){
+		e.stopPropagation();
+		e.preventDefault(); //return false;	
+
+		let fichero = e.dataTransfer.files[0];
+		let fr = new FileReader();
+
+		fr.onload = function(){
+			let img = new Image();
+			img.onload = function(){
+				let ctx = cv01.getContext('2d');
+				ctx.drawImage(img,0,0,cv01.width,cv01.height);
+			};
+			img.src = fr.result;
+		};
+		fr.readAsDataURL(fichero);
+	};
 }
